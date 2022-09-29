be1da69b236180c7c5e8241daabadad69cb150c6
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2022-09-28 20:55:57 -0400

    Comments
    Commented my code in Clock.ts

--- src/hardware/Clock.ts
+++ src/hardware/Clock.ts
@@ -4,7 +4,11 @@ import { System } from "../System"
 import { Hardware } from "./Hardware"
 import { ClockListener } from "./imp/ClockListener"
 
-
+/*
+* The Clock class represents the physical clock within the System. The Clock
+* keeps track of all parts of the system that listen for a clock pulse, and
+* handles initializing the pulses for said parts.
+*/
 export class Clock extends Hardware{
     //the listeners array is for storing all hardware that listens for a clock pulse
     private listeners: Array<ClockListener> = new Array<ClockListener>();
@@ -15,15 +19,17 @@ export class Clock extends Hardware{
         this.id = 0;
     }
 
+    //allows an outside class to add new parts to the listeners array
     public setCL(cl: ClockListener) {
         this.listeners.push(cl);
     }
 
+    //sends out a pulse by calling the pulse() method for every object in
+    //the listeners array
     public sendPulse() {
         this.log("Clock Pulse Initialized")
         for (let i = 0; i <= listeners.length; i++) {
             this.listeners[i].pulse();
         }
-    }
-    
+    }  
 }
